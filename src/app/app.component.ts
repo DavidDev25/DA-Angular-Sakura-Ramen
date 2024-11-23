3d2253a6a2318dcf576637dd4972c3d49cfb5323
DavidDev25 <david.werner@david-werner.dev>
2024-11-23 14:22:05 +0100

    added navbar in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landingPage/landingPage.component';
+import '@fontsource/raleway'; // Defaults to weight 400
+import '@fontsource/raleway/400.css'; // Specify weight
+import '@fontsource/raleway/400-italic.css'; // Specify weight and style
 
 @Component({
   selector: 'app-root',
--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-LandingPage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   template: `<section>
-    <h1 class="fontRaleway">SAKURA RAMEN</h1>
-    <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
+    <div><app-navbar> </app-navbar></div>
+    <h1 class="fontRalewayBold">SAKURA RAMEN</h1>
+    <h2 class="fontRaleway">THE BEST RAMEN IN TOWN</h2>
   </section>`,
   styleUrls: ['./landingPage.component.scss'],
 })
