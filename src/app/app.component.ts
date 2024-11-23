cfd85ac7b453b4c8a1000e2fe11b25884ac52c5f
DavidDev25 <david.werner@david-werner.dev>
2024-11-23 18:56:29 +0100

    Added Navbar, working on how to order section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,22 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landingPage/landingPage.component';
+import { OurMenuComponentComponent } from './our-menu-component/our-menu-component.component'; // Specify weight and style
 import '@fontsource/raleway'; // Defaults to weight 400
 import '@fontsource/raleway/400.css'; // Specify weight
-import '@fontsource/raleway/400-italic.css'; // Specify weight and style
-
+import '@fontsource/raleway/400-italic.css';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LandingPageComponent,
+    OurMenuComponentComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Sakura-Rahmen';
+  title = 'Sakura-Ramen';
 }
