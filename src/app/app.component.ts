268d4692ac1b155ffd5552a4737668b2deed5b87
DavidDev25 <david.werner@david-werner.dev>
2024-11-24 20:22:57 +0100

    added our Menu section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LandingPageComponent } from './landingPage/landingPage.component';
-import { HowToOrderComponent } from './howToOrder/how-to-order.component'; // Specify weight and style
-import '@fontsource/raleway'; // Defaults to weight 400
-import '@fontsource/raleway/400.css'; // Specify weight
-import '@fontsource/raleway/400-italic.css';
-import { OurMenuComponent } from './our-menu/our-menu.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    LandingPageComponent,
-    HowToOrderComponent,
-    OurMenuComponent,
-  ],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
