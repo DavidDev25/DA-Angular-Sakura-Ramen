a80c7b5721551b9d79f426d0d4350292bca5c742
DavidDev25 <david.werner@david-werner.dev>
2024-11-25 12:07:20 +0100

    added location section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landingPage.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
 import { HowToOrderComponent } from '../howToOrder/how-to-order.component';
+import { LocationSectionComponent } from '../location-section/location-section.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, OurMenuComponent, HowToOrderComponent],
+  imports: [
+    LandingPageComponent,
+    OurMenuComponent,
+    HowToOrderComponent,
+    LocationSectionComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
